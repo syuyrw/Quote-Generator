6f1d19a41397b77719eda8b5d96d04c638e8ba8e
syuyrw <jayreitz@gmail.com>
2025-08-14 17:55:21 -0600

    The program

---
+++ README.md
@@ -0,0 +1,39 @@
+# Quote Generator
+
+[![Live Demo](https://img.shields.io/badge/Live%20Demo-Drop%20Generator-blue?style=for-the-badge)](https://dropgenerator.com/)
+
+## Description
+
+I created this project because I was inspired by this project by @ImBaedin on GitHub: https://www.braedin.com/FortniteRandomDrop/
+
+I wanted to build off of his concept. The first thing that I wanted to add was the ability to make it so that you could generate a random POI instead of just a completely random location on the map. I often play with some of my friends, so it is more helpful to drop at a named location rather than a completely random point.
+
+The program is simple to use. All you have to do is either click the "Generate Drop" button to generate a random POI drop, or click "Random Spot" to generate a completely random point.
+
+## Getting Started
+
+Go to [dropgenerator.com](https://dropgenerator.com)
+
+### Technologies
+
+-   This programs UI was built using HTML and CSS and the program itsself was built in Javascript.
+
+## Help
+
+One thing to note is that you will need to press the "C" button after running a calculation to clear the screen before typing another equation.
+
+## Authors
+
+- @Tweagan11 implemented the completely random location generator, and also helped with the debugging for this project. Huge thanks to him.
+
+- Jordan Reitz (@syuyrw) jordancreitz@gmail.com
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+
+-   https://www.braedin.com/FortniteRandomDrop/
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ scripts.js
@@ -0,0 +1,88 @@
+const apiKey = "5jyhttcfhYQqiRuCvtVwVw==3mzIeqt70YflkdF8";
+const button = document.getElementById("button");
+const quoteText = document.getElementById("quote");
+const quoted = document.getElementById("quoted");
+const quoteArea = document.getElementById("quote-area");
+const tweetButton = document.getElementById("tweet");
+const linebreak = document.createElement("br");
+
+button.addEventListener("click", async () => {
+    // Clear any previous quote text
+    quoteText.textContent = "";
+    quoted.textContent = "";
+
+    // Unhide quote div if it is hidden
+    quoteArea.style.display = "block";
+
+    fetch(`https://api.api-ninjas.com/v1/quotes`, {
+        method: "GET",
+        headers: {
+            "X-Api-Key": apiKey,
+        },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.length > 0) {
+                console.log(`"${data[0].quote}", - ${data[0].author}`);
+                displayQuote(data[0].quote, data[0].author);
+                tweetButton.addEventListener("click", () => tweet(data[0].quote, data[0].author));
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching quote:", error);
+        });
+});
+
+
+// function to display quote and author
+function displayQuote(quote, author) {
+    quoteText.textContent = `"${quote}"`;
+    quoted.textContent = `- ${author}`;
+    // console.log(quote, quoted);
+}
+
+function tweet(quote, author) {
+    const tweetText = `"${quote}"\n- ${author}`;
+    const encodedTweetText = encodeURIComponent(tweetText);
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodedTweetText}`;
+    window.open(tweetUrl, '_blank');
+};
+
+const interval = setInterval(function() {
+    // Clear any previous quote text
+    quoteText.textContent = "";
+    quoted.textContent = "";
+
+    // Unhide quote div if it is hidden
+    quoteArea.style.display = "block";
+
+    fetch(`https://api.api-ninjas.com/v1/quotes`, {
+        method: "GET",
+        headers: {
+            "X-Api-Key": apiKey,
+        },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.length > 0) {
+                console.log(`"${data[0].quote}", - ${data[0].author}`);
+                displayQuote(data[0].quote, data[0].author);
+                tweetButton.addEventListener("click", () => tweet(data[0].quote, data[0].author));
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching quote:", error);
+        });
+}, 10000);
